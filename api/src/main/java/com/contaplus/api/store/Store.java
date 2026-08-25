ba715302ced84aa1b66ce4a67d4636a09854e4fe
Eduardo Silva <18013936+eduardonk9999@users.noreply.github.com>
2026-08-25 13:47:47 -0300

    mudando porta, iniciando o store

---
+++ api/src/main/java/com/contaplus/api/store/Store.java
@@ -0,0 +1,22 @@
+package com.contaplus.api.store;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "stores")
+public class Store {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private String currency;
+
+    
+
+}
