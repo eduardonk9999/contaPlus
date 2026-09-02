da50b5eb31678ca8459790b94595f65189c67940
Eduardo Silva <18013936+eduardonk9999@users.noreply.github.com>
2026-09-02 18:08:42 -0300

    testes

---
+++ api/src/test/java/com/contaplus/api/store/StoreRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.contaplus.api.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+@SpringBootTest
+@Import(TestConfiguration.class)
+public class StoreRepositoryTest {
+
+    @Autowired
+    StoreRepository repository;
+
+    @Test
+    void deveSalvarERecuperarUmaLoja() {
+        Store nova = new Store("Banca do Eduardo");
+
+        repository.save(nova);
+
+        Optional<Store> encontrada = repository.findById(nova.getId());
+
+        assertThat(encontrada).isPresent();
+        assertThat(encontrada.get().getName()).isEqualTo("Banca do Eduardo");
+    }
+}
