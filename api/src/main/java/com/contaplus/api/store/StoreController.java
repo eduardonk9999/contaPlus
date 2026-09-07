e58359ee19b21e65900da22139c1b7569aaf85eb
Eduardo Silva <18013936+eduardonk9999@users.noreply.github.com>
2026-09-07 11:33:43 -0300

    ajustando metodo response

--- api/src/main/java/com/contaplus/api/store/StoreController.java
+++ api/src/main/java/com/contaplus/api/store/StoreController.java
@@ -3,8 +3,10 @@ package com.contaplus.api.store;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
-@RequestMapping("/V1/stores")
+@RequestMapping("/v1/stores")
 public class StoreController {
     private final StoreService service;
 
@@ -14,10 +16,14 @@ public class StoreController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Store criar(@RequestBody CriarStoreRequest request) {
-        return service.criar(request.name());
+    public StoreResponse criar(@RequestBody CriarStoreRequest request) {
+        Store store = service.criar(request.name());
+        return new StoreResponse(store.getId(), store.getName());
     }
 
+    // DTO de entrada
     record CriarStoreRequest(String name) {}
 
+    // DTO de saida
+    record StoreResponse(UUID id, String name) {}
 }
